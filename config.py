5ef8421d4bd543f9f5bf5101f04c8d1980bc260a
kyle <yjkim308@gmail.com>
2022-08-23 13:58:01 +0900

    change config

--- config.py
+++ config.py
@@ -2,7 +2,12 @@
 #프로그렘 돌리기전에 파일 이름 바꿔야됨
 #파일 타입 무조건 포함해야됨 예) ".mp4"
 video_name = 'noobtest.mp4'
+
+#camera frames per second
+frame_per_second = 420
+
+#length of ball stand
+base_length = 1 #in meters
+
 box_start = (0,0)
 box_end = (0,0)
-frame_per_second = 420
-base_length = 1 #in meters
